1a5601c23f391ad201f19eac98ab50d7cdd2dc76
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-18 18:25:31 +0300

    add first less

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+let nameUser, age, surname;
+
+nameUser = "John";
+age = 21.5;
+surname = "Doe";
+let text = "test Test test"
+
+// text methods
+console.log(nameUser.toUpperCase())
+console.log(nameUser)
+console.log(nameUser.toLowerCase())
+console.log(nameUser.length)
+
+text = text.replaceAll(" ", "")
+console.log(text)
+
+
