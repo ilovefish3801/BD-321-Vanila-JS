e13a16da7f885f9574f6391aa1bbf2297598c112
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-22 17:25:23 +0300

    add first part

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,20 @@ const changeBgColor = (selector, color) => {
 document.addEventListener("DOMContentLoaded", ()=>{
     // get elements
     const RED_BTN = document.querySelector("#red");
+    // const NAME = document.querySelectorAll(".form__name")
+    // const NAME = document.getElementsByClassName("form__name")[0]
+    const NAME = document.querySelector(".form__name")
+    
     
     // add listeners
-    RED_BTN.addEventListener("click", (e)=>{
-        e.preventDefault();
-        changeBgColor("body", "#f00");
+    NAME.addEventListener("change", (e)=>{
+        const USER_NAME = e.target.value;
+        alert(USER_NAME)
     })
+    // RED_BTN.addEventListener("click", (e)=>{
+    //     e.preventDefault();
+    //     changeBgColor("body", "#f00");
+    // })
 })
 
 
