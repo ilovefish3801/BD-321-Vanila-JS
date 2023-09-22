c08b4afe32544985e65c12a7e49ca11f91eed542
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-22 17:34:46 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -9,13 +9,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const RED_BTN = document.querySelector("#red");
     // const NAME = document.querySelectorAll(".form__name")
     // const NAME = document.getElementsByClassName("form__name")[0]
-    const NAME = document.querySelector(".form__name")
+    const FORM = document.querySelector(".form")
     
     
     // add listeners
-    NAME.addEventListener("change", (e)=>{
-        const USER_NAME = e.target.value;
-        alert(USER_NAME)
+    // NAME.addEventListener("change", (e)=>{
+    //     const USER_NAME = e.target.value;
+    // })
+    FORM.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        // get elements
+        const NAME = document.querySelector(".form__name");
+        const USER_NAME = NAME.value;
+        console.log(USER_NAME)
+
     })
     // RED_BTN.addEventListener("click", (e)=>{
     //     e.preventDefault();
