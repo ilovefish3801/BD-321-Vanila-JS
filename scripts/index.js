c952616fcae9bd2a5e8bab961764f1167663eeb8
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-22 19:01:44 +0300

    add

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,20 @@ const changeBgColor = (selector, color) => {
 }
 
 // start point
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     // get elements
     const RED_BTN = document.querySelector("#red");
     // const NAME = document.querySelectorAll(".form__name")
     // const NAME = document.getElementsByClassName("form__name")[0]
     const FORM = document.querySelector(".form")
-    
-    
+    const SHOW_ELEMENT_BTN = document.querySelector(".show__element")
+
+
     // add listeners
     // NAME.addEventListener("change", (e)=>{
     //     const USER_NAME = e.target.value;
     // })
-    FORM.addEventListener("submit", (e)=>{
+    FORM.addEventListener("submit", (e) => {
         e.preventDefault();
         // get elements
         const NAME = document.querySelector(".form__name");
@@ -31,14 +32,26 @@ document.addEventListener("DOMContentLoaded", ()=>{
         NAME.value = "";
 
         // display status request
-        alert("You request sended!")
-        // console.log(USER_NAME, USER_COLOR)
+
 
     })
     // RED_BTN.addEventListener("click", (e)=>{
     //     e.preventDefault();
     //     changeBgColor("body", "#f00");
     // })
+    SHOW_ELEMENT_BTN.addEventListener("click", (e) => {
+        // generate html
+        let html = `
+            <h1>Test Insert Element</h1>
+            <div class="conntent__area_box">
+                test
+            </div>
+        `
+        // get insert area
+        const AREA = document.querySelector('.conntent__area')
+        // put html in area
+        AREA.innerHTML = html;
+    })
 })
 
 
